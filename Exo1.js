ca8abace37dffec0f52e0d1d6bcccfa0da881212
QuentinBensmaine <43670995+QuentinBensmaine@users.noreply.github.com>
2021-02-23 15:45:54 +0100

    Update Exo1.js

--- Exo1.js
+++ Exo1.js
@@ -1,13 +1,31 @@
+//Partie1
 let number1 = "2";
 let number2 = "5";
 
-add(number1, number2);
+addStrings(number1, number2);
 
-function add(a, b) {
+function addStrings(a, b) {
     let num1 = parseInt(a);
     let num2 = parseInt(b);
     let r = num1 + num2;
     let result = r.toString();
     console.log(result);
     return result;
+}
+
+//Partie2
+add("a", "b", 3);
+
+function add(a, b, c) {
+    if (typeof(a) != "number") {
+        a = 0;
+    }
+    if (typeof(b) != "number") {
+        b = 0;
+    }
+    if (typeof(c) != "number") {
+        c = 0;
+    }
+    let r = a + b + c;
+    console.log(r);
 }
